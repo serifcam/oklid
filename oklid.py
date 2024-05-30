0ea350841014472c95bfe8afdf223cd3d1d2a2bb
Şerif Çam <serifcam34@gmail.com>
2024-05-30 14:09:51 +0300

    first commit

---
+++ oklid.py
@@ -0,0 +1,21 @@
+import math
+
+# Noktaların Tanımlanması
+points = [(1, 2), (3, 4), (6, 8), (10, 10)]
+
+# Öklid Mesafesi İçin Bir Fonksiyon Yazma
+def euclideanDistance(point1, point2):
+    return math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+
+# Mesafelerin Hesaplanması
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+# Minimum Mesafenin Bulunması
+min_distance = min(distances)
+
+print("Mesafeler:", distances)
+print("Minimum Mesafe:", min_distance)
